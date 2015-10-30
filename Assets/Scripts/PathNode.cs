b173a646393134ef766de17a29cab6ef99264014
dafei2015 <649220234@qq.com>
2015-10-30 13:42:15 +0800

    增加路线节点和路线的生成

---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 节点类，定义了界面上的各个节点
+/// </summary>
+public class PathNode : MonoBehaviour
+{
+    public PathNode currentNode;
+    public PathNode nextNode;
+
+    /// <summary>
+    /// 设置节点
+    /// </summary>
+    /// <param name="node">下一个节点</param>
+    public void SetNode(PathNode node)
+    {
+        if (nextNode != null)
+        {
+            nextNode.currentNode = null;
+            nextNode = node;
+            currentNode = this;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PathRoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PathRoot:MonoBehaviour
+{
+    private GameObject[] mPaths;
+
+    [ContextMenu("显示线路")]
+    public void ShowPath()
+    {
+        mPaths = GameObject.FindGameObjectsWithTag("PathNode");
+    }
+
+    /// <summary>
+    /// 绘制图线
+    /// </summary>
+    public void OnDrawGizmos()
+    {
+        if (mPaths == null) return;
+
+        //设置Gizmos的颜色
+        Gizmos.color = Color.red;
+        foreach (GameObject go in mPaths)
+        {
+            PathNode node = go.GetComponent<PathNode>();
+            if (node == null) return;
+
+            if (node.nextNode != null)
+            {
+                Gizmos.DrawLine(node.transform.position, node.nextNode.transform.position);                
+            }
+            Gizmos.DrawCube(node.transform.position,new Vector3(0.1f,0.1f,0.1f));
+        }
+    }
+
+}
