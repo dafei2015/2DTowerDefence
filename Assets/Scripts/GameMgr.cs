45d6467db4d32de0effdd9e9707b8342bf5b26ec
dafei2015 <649220234@qq.com>
2015-10-30 13:44:11 +0800

    编写游戏管理类

---
+++ Assets/Scripts/GameMgr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameMgr:MonoBehaviour
+{
+    public static GameMgr instance;
+
+    /// <summary>
+    /// 怪物列表
+    /// </summary>
+    public ArrayList enemyList = new ArrayList();
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        EnemyNode[] arr = gameObject.GetComponentsInChildren<EnemyNode>();
+        foreach (EnemyNode e in arr)
+        {
+            enemyList.Add(e);
+        }
+    }
+}
