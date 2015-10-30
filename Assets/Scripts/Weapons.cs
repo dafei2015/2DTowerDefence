9439e2ba5da77d4120fe980d28529dcfbefd3d3c
dafei2015 <649220234@qq.com>
2015-10-30 13:44:55 +0800

    编写武器脚本,可以查询进入攻击范围内敌人

---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Weapons:MonoBehaviour
+{
+    /// <summary>
+    /// 攻击范围
+    /// </summary>
+    public float attackRange = 0.6f;
+
+    void Update()
+    {
+        FindEnemy();
+    }
+    /// <summary>
+    /// 查找敌人
+    /// </summary>
+    void FindEnemy()
+    {
+        ArrayList list = GameMgr.instance.enemyList;
+
+        foreach (EnemyNode enemy in list)
+        {
+            Vector3 pos1 = transform.position;
+            Vector3 pos2 = enemy.transform.position;
+
+            float dis = Vector2.Distance(new Vector2(pos1.x, pos1.y), new Vector2(pos2.x, pos2.y));
+
+            if (dis > attackRange)
+            {
+                Debug.Log(string.Format("敌人{0} 未进入攻击范围，距离：{1}",enemy.name,dis));
+            }
+            else
+            {
+                Debug.LogError(string.Format("敌人{0} 未进入攻击范围，距离：{1}", enemy.name, dis));
+            }
+        }
+    }
+}
