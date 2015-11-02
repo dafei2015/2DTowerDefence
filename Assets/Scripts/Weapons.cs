664822fba62dd1759f407fb00a6401b31dbad844
dafei2015 <649220234@qq.com>
2015-11-02 11:52:45 +0800

    绘制攻击范围及计算欧拉角实现武器对准敌人

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -8,9 +8,16 @@ public class Weapons:MonoBehaviour
     /// </summary>
     public float attackRange = 0.6f;
 
+    /// <summary>
+    /// 当前目标敌人
+    /// </summary>
+    public EnemyNode mTrg;
+
+
     void Update()
     {
         FindEnemy();
+        AimedAtEnemy();
     }
     /// <summary>
     /// 查找敌人
@@ -18,7 +25,7 @@ public class Weapons:MonoBehaviour
     void FindEnemy()
     {
         ArrayList list = GameMgr.instance.enemyList;
-
+        mTrg = null;
         foreach (EnemyNode enemy in list)
         {
             Vector3 pos1 = transform.position;
@@ -32,8 +39,50 @@ public class Weapons:MonoBehaviour
             }
             else
             {
-                Debug.LogError(string.Format("敌人{0} 未进入攻击范围，距离：{1}", enemy.name, dis));
+                if (enemy != mTrg) mTrg = enemy;
+                Debug.LogError(string.Format("敌人{0} 进入攻击范围，距离：{1}", enemy.name, dis));
             }
         }
     }
+
+    /// <summary>
+    /// 对准敌人
+    /// </summary>
+    void AimedAtEnemy()
+    {
+        if (mTrg == null) return;
+        Vector3 pos1 = transform.position;
+        Vector3 pos2 = mTrg.transform.position;
+
+        Vector3 dir = (pos2 - pos1).normalized;
+        //通过两点之间的法向量和向上的位置设置角度
+        float angle = GetAngle(Vector3.up, dir);
+        //设置旋转角度
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    /// <summary>
+    /// 获取欧垃角
+    /// </summary>
+    /// <param name="v1">第一个向量坐标</param>
+    /// <param name="v2">第二个坐标</param>
+    float GetAngle(Vector3 v1, Vector3 v2)
+    {
+        float dot = Vector3.Dot(v1, v2);
+        float mv1 = Mathf.Sqrt(v1.x*v1.x + v1.y*v1.y);
+        float mv2 = Mathf.Sqrt(v2.x*v2.x + v2.y*v2.y);
+
+        float angle = Mathf.Acos(dot/mv1*mv2)*Mathf.Rad2Deg;
+        //逆时针为正，顺时针为负
+        if (v2.x > v1.x) angle *= -1;
+        return angle;
+    }
+
+    //绘制攻击范围
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
 }
